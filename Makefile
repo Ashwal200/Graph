a74dfe765196672b7ff854f2c5b5c961167da50f
Ashwal200 <Ashwal200@gmail.com>
2023-01-19 12:50:17 +0200

    WIP

---
+++ Makefile
@@ -0,0 +1,38 @@
+CC = gcc -c
+CFLAGS = -Wall -g
+CLIBSA = ar rcs
+
+all: graph libstacts.a
+
+.PHONY:clean
+
+clean:
+	rm -rf *.o *.a graph
+
+graph : main.o libstacts.a
+	gcc -Wall main.o ./libstacts.a -o graph
+
+libstacts.a : edges.o nodes.o graph.o graph.h
+	$(CLIBSA) libstacts.a edges.o nodes.o graph.o
+
+main.o : main.c graph.h
+	$(CC) $(CFLAGS) main.c
+
+edges.o : edges.c graph.h edges.h
+	$(CC) $(CFLAGS) edges.c
+
+nodes.o : nodes.c graph.h
+	$(CC) $(CFLAGS) nodes.c
+
+graph.o : graph.c graph.h
+	$(CC) $(CFLAGS) graph.c
+
+
+#all: main
+#main: edges.c graph.c nodes.c main.c graph.h
+#	gcc -Wall -g -o graph edges.c graph.c nodes.c main.c
+#
+#.PHONY:clean
+#
+#clean:
+#	rm graph
---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "graph.h"
+
+
+
+#define END_OF_PROGRAM 0
+
+
+int main(){
+
+    char user_decision = ' ';
+    int answer =0;
+    pNode last_graph = NULL;
+    while ( !feof(stdin) )
+    {
+        user_decision = getchar();
+//        printf("the last user input after new loop is: %c\n", user_decision);//***********
+        switch (user_decision)
+        {
+            case 'A':
+//                printf("\nget into A \n");
+                if (last_graph != NULL)
+                {
+                    deleteGraph_cmd(&last_graph);
+                }
+                build_graph_cmd(&last_graph);
+//                user_decision = getchar();
+                break;
+
+            case 'B':
+//                printf("\nget into B \n");
+                insert_node_cmd(&last_graph);
+//                user_decision = getchar();
+                break;
+
+            case 'D':// Delete the next input node.
+//                printf("\nget into D \n");
+                delete_node_cmd(&last_graph);
+                break;
+
+            case 'S':
+                answer = shortsPath_cmd(last_graph);
+                printf("Dijsktra shortest path: %d\n", answer);
+                break;
+
+            case 'T':
+                answer= TSP_cmd(last_graph);
+                printf("TSP shortest path: %d\n", answer);
+//                user_decision = getchar();
+                break;
+
+            default:
+                printf("");
+        }
+    }
+    return END_OF_PROGRAM;
+}
